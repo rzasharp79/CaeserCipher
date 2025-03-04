1a25c51fe69a7c17eaf1d4ea19e7d2a51ab2696f
rzasharp79 <james.c.todd@gmail.com>
2025-03-04 17:21:46 -0500

    Initial commit

---
+++ CaesarCipher/CaesarCipherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaesarCipherApp.swift
+//  CaesarCipher
+//
+//  Created by James Todd on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CaesarCipherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaesarCipher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaesarCipher
+//
+//  Created by James Todd on 3/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
